9ee3effa8b014009fdd7b42b85ebad53f2ed1aa0
James Fowkes <jamesfowkes@gmail.com>
2016-08-18 16:58:08 +0100

    Remove unecessary prototype in Descriptors.h

--- Descriptors.h
+++ Descriptors.h
@@ -70,11 +70,4 @@
 		
 		#define FIXED_NUM_CONFIGURATIONS         1
 				
-	/* Function Prototypes: */
-		uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
-		                                    const uint8_t wIndex,
-		                                    const void** const DescriptorAddress)
-		                                    ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
-
 #endif
-
